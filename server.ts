bda7b24287f56821dbce612fb184f5637154699d
Eranga Hasakalum <erangahasakalum6761@gmail.com>
2025-02-27 17:19:47 +0530

    create server.ts

--- server.ts
+++ server.ts
@@ -7,14 +7,13 @@ app.use('/',(req,res,next)=>{
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, content-type');
-
     next();
 })
 
-app.listen(3000, (err=>{
+app.listen(3001, (err=>{
     console.log("Server running on port 3000");
 }));
 
 app.use('/',(req,res,next)=>{
-    res.status(200).send('Not Found');
+    res.status(200).send('server runing');
 })
